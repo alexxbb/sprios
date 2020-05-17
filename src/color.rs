719d486938a4c9d243829333db204233744394df
alexbb <hou.alexx@gmail.com>
2020-05-16 20:42:21 -0700

    WIP rendering to widget, but now rendering gives incorrect picture

--- src/color.rs
+++ src/color.rs
@@ -1,3 +1,23 @@
 use crate::utils::Clip;
 use crate::vec::Color;
 use std::rc::Rc;
+
+// pub fn write_color(
+//     writer: &Rc<dyn std::io::Write>,
+//     clr: &Color,
+//     samples_per_pixel: u32,
+// ) -> std::fmt::Result {
+//     let scale = 1.0 / samples_per_pixel as f32;
+//
+//     let r = (clr.x * scale).sqrt();
+//     let g = (clr.y * scale).sqrt();
+//     let b = (clr.z * scale).sqrt();
+//
+//     writeln!(
+//         writer,
+//         "{} {} {}",
+//         (256.0 * r.clip(0.0, 0.999)) as u32,
+//         (256.0 * g.clip(0.0, 0.999)) as u32,
+//         (256.0 * b.clip(0.0, 0.999)) as u32,
+//     )
+// }
--- src/main.rs
+++ src/main.rs
@@ -14,17 +14,19 @@ use gdk_pixbuf::{Colorspace, Pixbuf};
 use getopts;
 use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Button, Image};
+use gtk::{Application, ApplicationWindow, Button, Image, Orientation};
 use rand::Rng;
 use std::rc::Rc;
 use std::time::Instant;
 
 use crate::material::{Lambertian, Metal};
+use crate::utils::Clip;
 use camera::Camera;
 use hittable::Hittable;
 use ray::Ray;
 use sphere::Sphere;
 use std::cell::RefCell;
+use std::io::{BufWriter, Read, Write};
 use std::ops::DerefMut;
 use vec::*;
 
@@ -47,49 +49,73 @@ fn ray_color(ray: &Ray, world: &World, depth: u32) -> Color {
     Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t
 }
 
+#[allow(non_upper_case_globals)]
 fn build_ui(app: &gtk::Application) {
     let window = ApplicationWindow::new(app);
     window.set_title("Hello");
     // window.set_default_size(800, 400);
 
     let render_btn = Button::new_with_label("Render");
-    const image_width: u32 = 386;
     const aspect_ratio: f32 = 16.0 / 9.0;
-    const samples_per_pixel: u32 = 10;
+    const samples_per_pixel: u32 = 2;
+    let image_width: u32 = 386;
     let image_height: u32 = (image_width as f32 / aspect_ratio) as u32;
-    let cap = image_height * image_width * (std::mem::size_of::<u32>() * 3) as u32;
-    let mut buf = Rc::new(RefCell::new(ImageBuffer {
-        inner: Vec::with_capacity(cap as usize),
-    }));
+    let cap = (image_height * image_width * 3) as usize;
+    let mut buf = Rc::new(RefCell::new(ImageBuffer::new(
+        image_width,
+        image_height,
+        Vec::with_capacity(cap),
+    )));
+    let buf_rc = Rc::clone(&buf);
+    let image = Image::new();
+    let image_c = image.clone();
     render_btn.connect_clicked(move |_| {
-        render(image_width, image_height, samples_per_pixel, buf.clone());
+        buf_rc.borrow_mut().clear();
+        render(image_width, image_height, samples_per_pixel, buf_rc.clone());
+        assert_eq!(buf_rc.borrow().as_ref().len(), cap);
+        buf_rc.borrow_mut().debug();
+        use glib::Bytes;
+        let bytes = Bytes::from(buf_rc.borrow().as_ref());
+        let buf = Pixbuf::new_from_bytes(
+            &bytes,
+            Colorspace::Rgb,
+            false,
+            8 as i32,
+            image_width as i32,
+            image_height as i32,
+            3,
+        );
+        image_c.set_from_pixbuf(Some(&buf));
     });
 
-    // let image = Image::new_from_file("image.ppm");
-
-    let buf = Pixbuf::new(
-        Colorspace::Rgb,
-        true,
-        8,
-        image_width as i32,
-        image_height as i32,
-    );
-
-    window.add(&render_btn);
+    use gtk::BoxExt;
+    let vb = gtk::Box::new(Orientation::Vertical, 0);
+    vb.pack_start(&render_btn, true, true, 3);
+    vb.pack_end(&image, true, true, 3);
+    window.add(&vb);
     window.show_all()
 }
 
 struct ImageBuffer {
-    inner: Vec<f32>,
+    inner: Vec<u8>,
+    width: u32,
+    height: u32,
 }
 
-impl AsRef<[f32]> for ImageBuffer {
-    fn as_ref(&self) -> &[f32] {
+impl AsRef<[u8]> for ImageBuffer {
+    fn as_ref(&self) -> &[u8] {
         &self.inner
     }
 }
 
 impl ImageBuffer {
+    pub fn new(width: u32, height: u32, buf: impl Into<Vec<u8>>) -> ImageBuffer {
+        ImageBuffer {
+            inner: buf.into(),
+            width,
+            height,
+        }
+    }
     pub fn write_color(&mut self, clr: &Color, samples_per_pixel: u32) {
         let scale = 1.0 / samples_per_pixel as f32;
 
@@ -97,9 +123,31 @@ impl ImageBuffer {
         let g = (clr.y * scale).sqrt();
         let b = (clr.z * scale).sqrt();
 
-        self.inner.push(r);
-        self.inner.push(g);
-        self.inner.push(b);
+        self.inner.push((256.0 * r.clip(0.0, 0.999)) as u8);
+        self.inner.push((256.0 * g.clip(0.0, 0.999)) as u8);
+        self.inner.push((256.0 * b.clip(0.0, 0.999)) as u8);
+    }
+
+    pub fn clear(&mut self) {
+        self.inner.clear()
+    }
+
+    pub fn debug(&self) {
+        use std::fs::File;
+        let mut f = File::create("image.ppm").expect("Could not create ppm");
+        let mut buf = BufWriter::with_capacity(self.inner.len(), &f);
+
+        writeln!(buf, "P3\n{} {}\n255", self.width, self.height);
+
+        for i in 0..self.inner.len() - 2 {
+            buf.write_fmt(format_args!(
+                "{} {} {}\n",
+                self.inner[i],
+                self.inner[i + 1],
+                self.inner[i + 2]
+            ));
+        }
+        buf.flush();
     }
 }
 
@@ -161,7 +209,6 @@ fn render(width: u32, height: u32, samples: u32, mut buf: Rc<RefCell<ImageBuffer
             buf.write_color(&pixel_color, samples);
         }
     }
-    println!("Render done");
 }
 
 #[allow(non_upper_case_globals)]
