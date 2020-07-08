8d312ec0a6d8c1f691c6ebd4abfc8dbece718b41
Aleksei Rusev <arusev@imageworks.com>
2020-07-08 11:47:42 -0700

    World from file

--- src/renderer/src/world.rs
+++ src/renderer/src/world.rs
@@ -1,12 +1,15 @@
 use crate::hittable::{HitRecord, Hittable};
-use crate::{Ray};
+use crate::{Ray, Camera};
 use std::sync::Arc;
 use crate::bbox::AaBb;
+use std::path::Path;
+use std::io::Read;
 
 trait Foo: Send + Sync {}
 
 pub struct World {
     pub(crate) objects: Vec<Arc<dyn Hittable>>,
+    pub(crate) camera: Camera,
 }
 
 impl Hittable for World {
@@ -50,7 +53,7 @@ impl Hittable for World {
 
 impl World {
     pub fn new() -> World {
-        World { objects: vec![] }
+        World { objects: vec![], camera: Camera::default()}
     }
     /*
         Why 'static is needed here??
@@ -61,15 +64,42 @@ impl World {
     pub fn add(&mut self, object: impl Hittable + 'static) {
         self.objects.push(Arc::new(object))
     }
+
+    pub fn from_file(path: impl AsRef<Path>) -> Result<World, String> {
+        match std::fs::File::open(path)
+        {
+            Err(e) => return Err(e.to_string()),
+            Ok(mut file) => {
+                let mut content= String::new();
+                file.read_to_string(&mut content);
+
+                for line in content.lines() {
+                    if line.starts_with('#') || line.is_empty() {
+                        continue
+                    }
+                    if let Ok(cam) = line.parse::<Camera>() {
+                        dbg!(cam);
+                    }
+                    else {
+                        eprintln!("Could not parse line: {}", line);
+                    }
+                }
+            }
+
+        }
+        Ok(World::new())
+    }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{Sphere, Lambertian, Color};
+    use crate::{Sphere, Lambertian, Color, Point3};
+    use std::path::Path;
 
     #[test]
-    fn test() {
+    fn test_1() {
         let mut world = World::new();
         world.add(
             Sphere::new((0.0, 0.0, 0.0), 0.5, Box::new(Lambertian { color: Color::ONE })),
@@ -85,6 +115,12 @@ mod tests {
         );
         let bbox = world.bbox(0.0, 0.0).unwrap();
         assert_eq!(&bbox.max, &Point3::new(1.5, 1.5, 0.5));
+    }
 
+    #[test]
+    fn test_read_file() {
+        assert!(World::from_file(Path::new("Foo")).is_err());
+        let sc_1 = Path::new("../scene_1.rsc");
+        assert!(World::from_file(sc_1).is_ok());
     }
 }
---
+++ src/scene_1.rsc
@@ -0,0 +1,37 @@
+## simple scene
+camera 20 10 10 0 1 0 30 0.04
+
+background 0.3 0.6 0.9
+
+emit 0.9 0.6 0.3 100
+sphere -10 10 0 1
+
+diffuse 0.5 0.5 0.5
+sphere  0.0 1.0  0.0  1.0
+
+diffuse 0.2 0.8 0.2
+sphere -4 1 0 1
+
+diffuse 0.2 0.2 0.8
+sphere 4 1 0 1
+
+metal 0.5 0.5 0.8 0.0
+sphere -4 2 4 2
+
+diffuse 0.8 0.2 0.2
+cube 0 1 4 1
+
+metal 0.8 0.8 0.2 0.0
+sphere 4 1.5 4 1.5
+
+diffuse 0.8 0.4 0.2
+sphere 0 1 -4 1
+
+glass 1.5
+sphere -4 2 -4 2
+
+diffuse 0.1 0.1 0.1
+sphere 4 1 -4 1
+
+diffuse 0.5 0.5 0.5
+cube 0 -200 0 200
